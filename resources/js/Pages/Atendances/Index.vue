62d4aabc512a79a31fbbcb367b3d5076a6eac802
Ogilo G.D. Ouma <gogilo2003@hotmail.com>
2024-01-16 16:45:14 +0300

    Adding Andendance component

---
+++ resources/js/Pages/Atendances/Index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+</script>
+<template>
+    <AuthenticatedLayout title="Staff Members"></AuthenticatedLayout>
+</template>
