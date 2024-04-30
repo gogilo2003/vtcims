a11538a0bcfee0a0e843b28e726d32576f5095cf
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2024-04-30 09:52:58 +0300

    Updated the view blade to include examination summary and a styling

--- resources/views/pdf/students/view.blade.php
+++ resources/views/pdf/students/view.blade.php
@@ -1,72 +1,131 @@
 @extends('layout.pdf')
 
 @section('title')
-    E-School::View ({{ $student->name }})
+    E-School::View ({{ $student->first_name }}{{ $student->middle_name ? ' ' . $student->middle_name : '' }}
+    {{ $student->surname }})
 @endsection
 
 @section('content')
-    <table class="table table-no-border">
+    <table class="w-full mt-3">
         <tr>
-            <td width="15%" style="vertical-align: top;">
-                <img src="{{ file_exists('images/students/' . $student->photo) && $student->photo ? asset(config('admin.path_prefix') . '/images/students/' . $student->photo) : asset(config('admin.path_prefix') . '/vendor/admin/img/person_8x10.png') }}"
-                    alt="" class="img-fluid">
+            <td width="30%" style="vertical-align: top;" class="text-center pr-3">
+                <img src="{{ $student->photo_url }}" alt="{{ $student->photo_url }}"
+                    class="w-full border border-gray-800 rounded-lg overflow-hidden p-1">
+                <div class="uppercase mt-4 font-light text-gray-800">Name:</div>
+                <div class="uppercase font-medium text-lg">
+                    {{ $student->first_name }}{{ $student->middle_name ? ' ' . $student->middle_name : '' }}
+                    {{ $student->surname }}</div>
+                <div class="uppercase mt-4 font-light text-gray-800">Admission Number:</div>
+                <div class=" font-medium text-lg">{{ $student->admission_no }}</div>
             </td>
-            <td width="85%">
-                <table class="table text-uppercase table-bordered table-striped">
+            <td width="70%">
+                <table class="w-full">
                     <tr>
-                        <th>Name</th>
-                        <th>Admission No</th>
-                        <th>Class/Intake</th>
-                        <th>Course</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">
+                            Class/Intake</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Course
+                        </th>
                     </tr>
                     <tr>
-                        <td>{{ $student->name }}</td>
-                        <td>{{ $student->admission_no }}</td>
-                        <td>{{ $student->intake->name }}</td>
-                        <td>{{ $student->intake->course->name }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->intake->name }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->intake->course }}</td>
                     </tr>
                     <tr>
-                        <th>Idno</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Gender</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Idno</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Email
+                        </th>
                     </tr>
                     <tr>
-                        <td>{{ $student->idno }}</td>
-                        <td>{{ $student->email }}</td>
-                        <td>{{ $student->phone }}</td>
-                        <td>{{ $student->gender ? 'Female' : 'Male' }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->idno }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->email }}</td>
                     </tr>
                     <tr>
-                        <th>Postal Address</th>
-                        <th>Date of Birth</th>
-                        <th>Birth Certificate Number</th>
-                        <th>Date of Admission</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Phone
+                        </th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Gender
+                        </th>
                     </tr>
                     <tr>
-                        <td>{{ $student->address }}</td>
-                        <td>{{ $student->date_of_birth }}</td>
-                        <td>{{ $student->birth_cert_no }}</td>
-                        <td>{{ $student->date_of_admission }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->phone }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->gender ? 'Female' : 'Male' }}</td>
                     </tr>
                     <tr>
-                        <th>Sponsor</th>
-                        <th>Program</th>
-                        <th colspan="2">Role</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Postal
+                            Address</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Date of
+                            Birth</th>
                     </tr>
                     <tr>
-                        <td>{{ $student->sponsor->name }}</td>
-                        <td>{{ $student->program->name }}</td>
-                        <td colspan="2">{{ $student->role->name }}</td>
+                        <td class="px-2 py-2 border border-gray-800">P.O. Box
+                            {{ $student->box_no }}{{ $student->post_code ? ' ' . $student->post_code : '' }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->date_of_birth }}</td>
                     </tr>
                     <tr>
-                        <th colspan="4">Physical Address</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Birth
+                            Certificate
+                            Number</th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Date of
+                            Admission
+                        </th>
                     </tr>
                     <tr>
-                        <td colspan="4">{!! $student->physical_address ? $student->physical_address : '&nbsp;' !!}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->birth_cert_no }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->date_of_admission }}</td>
+                    </tr>
+                    <tr>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Sponsor
+                        </th>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase">Program
+                        </th>
+                    </tr>
+                    <tr>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->sponsor->name }}</td>
+                        <td class="px-2 py-2 border border-gray-800">{{ $student->program->name }}</td>
+                    </tr>
+                    <tr>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase"
+                            colspan="2">Role
+                        </th>
+                    </tr>
+                    <tr>
+                        <td colspan="2" class="px-2 py-2 border border-gray-800">{{ $student->role->name }}</td>
+                    </tr>
+                    <tr>
+                        <th class="px-2 py-2 border border-gray-800 text-left bg-gray-800 text-gray-100 uppercase"
+                            colspan="4">
+                            Physical Address
+                        </th>
+                    </tr>
+                    <tr>
+                        <td class="px-2 py-2 border border-gray-800" colspan="4">{!! $student->physical_address ? $student->physical_address : '&nbsp;' !!}</td>
                     </tr>
                 </table>
             </td>
         </tr>
     </table>
+    <div class="mt-6 pt-3 border-t">
+        <div class="text-xl font-light mb-3 uppercase">Examination Summary</div>
+        <table class="w-full border-collapse text-sm border-spacing-0 border-t border-b border-gray-800">
+            <thead class="uppercase">
+                <tr>
+                    <th class="border-l border-r border-gray-800 bg-gray-800 px-3 py-2 text-gray-100">#</th>
+                    <th class="border-r border-gray-800 bg-gray-800 px-3 py-2 text-gray-100 text-left">Subject</th>
+                    <th class="border-r border-gray-800 bg-gray-800 px-3 py-2 text-gray-100">Mean</th>
+                    <th class="border-r border-gray-800 bg-gray-800 px-3 py-2 text-gray-100">Min</th>
+                    <th class="border-r border-gray-800 bg-gray-800 px-3 py-2 text-gray-100">Max</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($examination as $exam)
+                    <tr class="even:bg-gray-200 font-light">
+                        <td class="border-l border-r border-gray-800 px-3 py-2 text-center">{{ $loop->iteration }}</td>
+                        <td class="border-r border-gray-800 px-3 py-2">{{ $exam->subject }}</td>
+                        <td class="border-r border-gray-800 px-3 py-2 text-center">{{ $exam->average }}</td>
+                        <td class="border-r border-gray-800 px-3 py-2 text-center">{{ $exam->min }}</td>
+                        <td class="border-r border-gray-800 px-3 py-2 text-center">{{ $exam->max }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
