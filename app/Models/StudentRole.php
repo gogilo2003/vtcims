8d76f1a76a49c97685d1710182c42aa8c8d4f6af
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2024-04-30 10:56:59 +0300

    Updated relationship comment and modernised

--- app/Models/StudentRole.php
+++ app/Models/StudentRole.php
@@ -3,11 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StudentRole extends Model
 {
-    public function students()
+
+    /**
+     * Get all of the students for the StudentRole
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function students(): HasMany
     {
-        return $this->hasMany('App\Models\Student', 'student_role_id', 'id');
+        return $this->hasMany(Student::class, 'student_role_id', 'id');
     }
 }
