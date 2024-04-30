aa3c751530114b137130f74e841e58c9ed990eb8
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2024-04-30 06:16:51 +0300

    Revised students subjects and scores listing

--- resources/views/pdf/examinations/examination.blade.php
+++ resources/views/pdf/examinations/examination.blade.php
@@ -9,7 +9,7 @@
         <thead class="bg-gray-500 text-gray-50">
             <tr>
                 <th class="border-l border-r border-gray-800 px-3 py-2" style="width:50px; text-align:center"></th>
-                <th class="border-r border-gray-800 px-3 py-2" style="width:150px; text-align:center">Admission No</th>
+                <th class="border-r border-gray-800 px-3 py-2 w-56">Admission No</th>
                 <th class="border-r border-gray-800 px-3 py-2 text-left">Student Name</th>
                 @foreach ($examination->tests as $test)
                     <th class="border-r border-gray-800 px-3 py-2" style="width:120px; text-align:center">
@@ -18,49 +18,31 @@
                 @endforeach
 
                 @if (!isset($blank))
-                    <th class="border-r border-gray-800 px-3 py-2" style="width:120px; text-align:center">Grade</th>
-                    <th class="border-r border-gray-800 px-3 py-2" style="width:120px; text-align:center">Remarks</th>
+                    <th class="border-r border-gray-800 px-3 py-2 w-20">Grade</th>
+                    <th class="border-r border-gray-800 px-3 py-2 text-left" style="width:120px;">Remarks</th>
                 @endif
             </tr>
         </thead>
         <tbody>
-            @php
-                $i = 0;
-            @endphp
-            {{-- @foreach ($examination->intakes as $intake) --}}
             @foreach ($examination->students as $student)
                 <tr class="even:bg-gray-100">
-                    <td class="text-center border-l border-r border-gray-800 px-3 py-2">{{ ++$i }}</td>
+                    <td class="text-center border-l border-r border-gray-800 px-3 py-2">{{ $loop->iteration }}</td>
                     <td class="text-center border-r border-gray-800 px-3 py-2">{{ $student->admission_no }}</td>
                     <td class="border-r border-gray-800 px-3 py-2">{{ $student->name }}</td>
-                    @php
-                        $grade = null;
-                        $remarks = null;
-                        $total = 0;
-                    @endphp
-                    @if (isset($blank))
-                        <td class="text-center border-r border-gray-800 px-3 py-2"></td>
-                    @else
-                        @foreach ($examination->tests as $test)
-                            @php
-
-                            @endphp
-                            @forelse ($student->results->where('test_id',$test->id) as $result)
-                                @php
-                                    $score = $result->score; //round($result->score / $test->outof * 100);
-                                    $total += $score;
-                                @endphp
-                                <td class="text-center border-r border-gray-800 px-3 py-2">{{ $score }}</td>
-                            @empty
+                    @foreach ($student->results as $result)
+                        @if (isset($blank))
+                            <td class="text-center border-r border-gray-800 px-3 py-2"></td>
+                        @else
+                            @if ($result->score)
+                                <td class="text-center border-r border-gray-800 px-3 py-2">{{ $result->score }}</td>
+                            @else
                                 <td class="text-center border-r border-gray-800 px-3 py-2">-</td>
-                            @endforelse
-                            @php
-                                $grade = do_grade($total);
-                                $remarks = do_remarks($grade);
-                            @endphp
-                        @endforeach
-                        <td class="text-center border-r border-gray-800 px-3 py-2">{{ $grade }}</td>
-                        <td class="text-center border-r border-gray-800 px-3 py-2">{{ $remarks }}</td>
+                            @endif
+                        @endif
+                    @endforeach
+                    @if (!isset($blank))
+                        <td class="text-center border-r border-gray-800 px-3 py-2">{{ $student->grade }}</td>
+                        <td class="border-r border-gray-800 px-3 py-2">{{ $student->remarks }}</td>
                     @endif
                 </tr>
             @endforeach
