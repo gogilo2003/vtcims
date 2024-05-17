37f7bf99f7095536d17561dbef5cc7428b92a473
Ogilo G.D. Ouma <gogilo2003@hotmail.com>
2024-05-17 03:47:26 +0300

    added checking for admins table existence

--- app/Console/Commands/CopyUsers.php
+++ app/Console/Commands/CopyUsers.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class CopyUsers extends Command
 {
@@ -27,22 +28,24 @@ class CopyUsers extends Command
      */
     public function handle()
     {
-        if (DB::table('users')->count() === 0) {
-            DB::table('admins')->get()->each(function ($admin) {
-                $role = \App\Models\Role::find($admin->admin_role_id);
-                $admin_role = DB::table('admin_roles')->find($admin->admin_role_id);
+        if (Schema::hasTable('admins')) {
+            if (DB::table('users')->count() === 0) {
+                DB::table('admins')->get()->each(function ($admin) {
+                    $role = \App\Models\Role::find($admin->admin_role_id);
+                    $admin_role = DB::table('admin_roles')->find($admin->admin_role_id);
 
-                $is_admin = (Str::contains($admin_role->name, 'Super') || Str::contains($admin_role->name, 'Admin')) ? 1 : 0;
+                    $is_admin = (Str::contains($admin_role->name, 'Super') || Str::contains($admin_role->name, 'Admin')) ? 1 : 0;
 
-                $user = new \App\Models\User();
-                $user->name = $admin->name;
-                $user->email = $admin->email;
-                $user->password = $admin->password;
-                $user->is_admin = $is_admin;
-                $role->users()->save($user);
-            });
-        } else {
-            $this->info('Users table already has data. Skipping...');
+                    $user = new \App\Models\User();
+                    $user->name = $admin->name;
+                    $user->email = $admin->email;
+                    $user->password = $admin->password;
+                    $user->is_admin = $is_admin;
+                    $role->users()->save($user);
+                });
+            } else {
+                $this->info('Users table already has data. Skipping...');
+            }
         }
         return 0;
     }
