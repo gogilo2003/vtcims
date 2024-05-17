d7aceb6eb4b3ce25c166682b6600f0a363d92c4b
Ogilo G.D. Ouma <gogilo2003@hotmail.com>
2024-05-17 05:37:14 +0300

    Added transactions summary to Accounts dashboard

--- app/Http/Controllers/V1/AccountController.php
+++ app/Http/Controllers/V1/AccountController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\V1;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Support\Str;
+use App\Models\FeeTransaction;
+use App\Models\FeeTransactionType;
+use App\Http\Controllers\Controller;
 
 class AccountController extends Controller
 {
@@ -14,6 +16,16 @@ class AccountController extends Controller
      */
     function index()
     {
-        return Inertia::render('Accounts/Index');
+
+        $stats = [];
+
+        foreach (FeeTransactionType::all() as $type) {
+            $stats = [
+                "title" => Str::title(Str::lower($type->description)),
+                "amount" => $type->fee_transactions->sum('amount')
+            ];
+        }
+
+        return Inertia::render('Accounts/Index', ['stats' => $stats]);
     }
 }
--- resources/js/Pages/Accounts/Index.vue
+++ resources/js/Pages/Accounts/Index.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
+import { formatCurrency } from '@/helpers';
 import AuthenticatedLayout from '../../Layouts/AuthenticatedLayout.vue'
 
+const props = defineProps<{
+    stats: {
+        title: string
+        amount: number
+    }[]
+}>()
 </script>
 <template>
     <AuthenticatedLayout title="Accounts Dashboard">
-        Accounts Dashboard
+        <div class="flex gap-4 w-full">
+            <div class="p-3 rounded-xl bg-white dark:bg-gray-800" v-for="{ title, amount } in stats">
+                <div class="text-primary-500 text-2xl font-light" v-text="formatCurrency(amount)"></div>
+                <div class="text-sm font-semibold uppercase" v-text="title"></div>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
